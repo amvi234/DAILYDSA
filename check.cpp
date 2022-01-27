781cfb072474052d21098843e9c8472f5d008977
Amit Vikram <amvi19cs@cmrit.ac.in>
2022-01-27 19:16:08 +0530

    check

---
+++ check.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter a & b"<<endl;
+    cin>>a>>b;
+       int sum = a+b;
+       cout<<"Sum is: "<<sum;
+    return 0;
+}
