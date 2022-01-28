4f1cf5543745de6b5777b484cb887527030aff30
Amit Vikram <amvi19cs@cmrit.ac.in>
2022-01-28 20:54:08 +0530

    comments

--- handshake.cpp
+++ handshake.cpp
@@ -7,14 +7,15 @@ class Solution{
 public:
     int count(int N){
         // code here
+        // N=6
         vector<int>dp(N+1,0);
         dp[0]=1;
         for(int i=0;i<=N;i+=2){
             for(int j=0;j<=i-2;j+=2 ){
-                dp[i] += dp[j]*dp[i-j-2];
+                dp[i] += dp[j]*dp[i-j-2];//1+2*2
             }
         }
-    return dp[N];
+    return dp[N]; //return 5
         
     }
     
