8402ecfa8abe2b9ab5a74280d4165a4f4d9a96ba
Amit Vikram <amvi19cs@cmrit.ac.in>
2022-09-05 06:43:22 +0530

    dsa1

---
+++ Arrays/maxsubarray.java
@@ -0,0 +1,20 @@
+public int maxSubArray(int[] nums) {
+    int sum=0;
+    int maxsum=Integer.MIN_VALUE;
+    for(int i=0;i<nums.length;i++){
+           
+        
+            sum+=nums[i];
+            if(sum>maxsum){
+                maxsum=sum;
+            }
+        
+            if(sum<=0){
+               sum=0;
+            }
+            
+        
+        System.out.print(maxsum+" ");
+    }
+    return maxsum;
+}
