18de852bdcab1410a0607ec9821138391cc5f3cf
Amit Vikram <amvi19cs@cmrit.ac.in>
2023-02-03 22:25:04 +0530

    krushkals

--- check.cpp
+++ check.cpp
@@ -1,30 +1,42 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 int main(){
-    int n,r;
-    cin>>n>>r;
+    int s=0;
+    int ans=0;
+    int a[3]={1,-1,4};
+    sort(a,a+3);
+       for(int i=1;i<3;i++){
+        s=abs(a[i-1]-a[i]);
+        ans+=s;
+        cout<<s<<endl;
+       }
+       cout<<ans;
+    //    cout<<s;
+    // int n,r;
+    // cin>>n>>r;
 
-    long long int val=1;
-    long long int val1=1;
-    long long int val2=1;
-    for(int i=n;i>=1;i--){
-        val *= i; 
-    }
-    cout<<"val is  "<<val<<endl;
-    for(int i=r;i>=1;i--){
-        val1 *= i; 
-    }
-    cout<<"val1 is  "<<val1<<endl;
-    int o = n-r;
+    // long long int val=1;
+    // long long int val1=1;
+    // long long int val2=1;
+    // for(int i=n;i>=1;i--){
+    //     val *= i; 
+    // }
+    // cout<<"val is  "<<val<<endl;
+    // for(int i=r;i>=1;i--){
+    //     val1 *= i; 
+    // }
+    // cout<<"val1 is  "<<val1<<endl;
+    // int o = n-r;
 
 
-    for(int i=o;i>=1;i--){
-        val2 *= i; 
-    }
-    cout<<"val2 is  "<<val2<<endl;
-    long long x = val/(val2*val1);
-    cout<<x; 
-    ////
+    // for(int i=o;i>=1;i--){
+    //     val2 *= i; 
+    // }
+    // cout<<"val2 is  "<<val2<<endl;
+    // long long x = val/(val2*val1);
+    // cout<<x; 
+    // ////
 
 
 
