e289b4437c8af3c14da50200dd06de14e18289b7
Amit Vikram <amvi19cs@cmrit.ac.in>
2023-02-06 19:45:12 +0530

    recursio

---
+++ recursion/fibonacci.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int func(int n){
+    if(n==1){
+        return 1;
+    }
+    if(n==0){
+        return 0;
+    }
+    return func(n-1)+func(n-2);
+}
+int main(){
+    int n;
+    cin>>n;
+    cout<< func(n)<<endl;
+    return 0;
+}
