9004f621c25c725a0d8f1386fb03301ee6c64206
Amit Vikram <amvi19cs@cmrit.ac.in>
2023-02-11 12:12:22 +0530

    dp

--- DP/minCostforTickets.cpp
+++ DP/minCostforTickets.cpp
@@ -1,5 +1,35 @@
 class Solution {
 public:
+//SPACE OPTIMIZATION - S.O:- O(1) constant
+// T.C:- O(N)
+int solveSpaceOP(int n, vector<int> &days, vector<int> &cost){
+    
+    int ans = 0;
+    
+    queue<pair<int, int>> month;
+    queue<pair<int, int>> week;
+    
+    for(int day: days){
+        // step-1 remove expired days      
+        while(!month.empty() && month.front().first + 30 <= day){
+            month.pop();
+        }
+  
+        while(!week.empty() && week.front().first + 7 <= day){
+            week.pop();
+        }
+        
+        // step-2 add cost for current day
+        week.push(make_pair(day, ans + cost[1]));
+        month.push(make_pair(day, ans + cost[2]));
+        
+        // step - 3 ans update
+        ans = min(ans + cost[0], min(week.front().second,month.front().second));
+        
+    }
+    
+    return ans;
+}
 //TABULATION
 int solve(vector<int>& days, vector<int>& costs){
         vector<int>dp(days.size()+1,INT_MAX);
