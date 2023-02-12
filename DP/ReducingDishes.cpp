1f7c57c7467829dacfe9a23ccadc1419953bd1d5
Amit Vikram <amvi19cs@cmrit.ac.in>
2023-02-12 21:33:04 +0530

    dp

--- DP/ReducingDishes.cpp
+++ DP/ReducingDishes.cpp
@@ -12,9 +12,35 @@ public:
          dp[index][time]= max(include,exclude);
          return dp[index][time];
     }
+    int solvetab(vector<int>& satisfaction){
+         vector<vector<int>>dp(satisfaction.size()+1,vector<int>(satisfaction.size()+1,0));
+         for(int index=satisfaction.size()-1;index>=0;index--){
+             for(int time=index;time>=0;time--){
+                 int i=satisfaction[index]*(time+1)+dp[index+1][time+1];
+                 int e=dp[index+1][time];
+                 dp[index][time]=max(i,e);
+             }
+         }
+         return dp[0][0];
+    }
+    int solvetab2(vector<int>& s){
+        //  vector<vector<int>>dp(satisfaction.size()+1,vector<int>(satisfaction.size()+1,0));
+        vector<int>curr(s.size()+1,0);
+        vector<int>next(s.size()+1,0);
+
+         for(int index=s.size()-1;index>=0;index--){
+             for(int time=index;time>=0;time--){
+                 int i=s[index]*(time+1)+next[time+1];
+                 int e=next[time];
+                 curr[time]=max(i,e);
+             }
+             next=curr;
+         }
+         return next[0];
+    }
     int maxSatisfaction(vector<int>& satisfaction) {
         sort(satisfaction.begin(),satisfaction.end());
-        vector<vector<int>>dp(satisfaction.size(),vector<int>(satisfaction.size()+1,-1));
-        return solve(satisfaction,0,1,dp);
+        // vector<vector<int>>dp(satisfaction.size(),vector<int>(satisfaction.size()+1,-1));
+        return solvetab2(satisfaction);
     }
 };
